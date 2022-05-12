cc1a93a77c15060130100095eb89a6817c23efb4
fr4nc1sj0hn <francisjohnpicaso@gmail.com>
2022-05-12 21:15:49 +0800

    Added Config Support and Local CSV Support

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+SP_cfg = {
+    "SharePoint_BaseSite": r"imecinternational.sharepoint.com",
+    "SharePoint_TargetSite": r"/sites/Team-ToolData",
+    "SharePoint_DriveName": r"Documents",
+    "SharePoint_Path": {
+        "eDR": r"/KLA_eDR/",
+        "CDSEM": r"/Hitachi_CD-SEMs/"
+    },
+    "SharePoint_AccessAccount": "appm_con@imecinternational.onmicrosoft.com",
+    
+    "AD_App_client_id": r"415650c1-1005-494c-b25d-08dc96807886",
+    "AD_App_authority": r"https://login.microsoftonline.com/a72d5a72-25ee-40f0-9bd1-067cb5b770d4",
+}
+
+logfolderlocation = "C:/Users/Francis John Picaso/Repositories/Teo/logs/"
+isFolder = 'Y'
+LocalCSVLocation = "C:/Users/Francis John Picaso/Repositories/Teo/Output/"
+UploadToSharePoint = "N"
--- logParser.py
+++ logParser.py
@@ -1,13 +1,22 @@
 import pandas as pd
+import os
 from os import listdir
 from os.path import isfile, join
 import sys
 import warnings
+import config as cfg
+import datetime
+import time
 
 class LogParser():
-    def __init__(self,path,isFolder):
+    def __init__(self,path,isFolder, LocalCSVLocation, SharepointLocation,UploadToSharePoint):
         self.path = path
         self.isFolder = isFolder
+        self.LocalCSVLocation = LocalCSVLocation
+        self.SharepointLocation = SharepointLocation
+        self.UploadToSharePoint = UploadToSharePoint
+
+
         columns = ['Timestamp', 'Log Message', 'Message', 'User Path', 'Customer Path','Lot ID', 'Wafer ID', 'Port ID', 'Date', 'Time']
         df = pd.DataFrame(columns=columns)
         self.parsedLog = df
@@ -53,115 +62,136 @@ class LogParser():
             & (step1["Log Message"].str.endswith('>',na=False))
         ]
         
-        #step3 Table.ReplaceValue("Lot Run - Dumping special environment variables:<Lot Run -Lot name : ",
-        #"Lot Start <",
-        #Replacer.ReplaceText,{"Log Message"})
-        step2["Log Message"] = step2["Log Message"].str.replace(
-            "Lot Run - Dumping special environment variables:<Lot Run -Lot name : ",
-            "Lot Start <"
-        )
-        
-        #step4 Table.ReplaceValue(
-        #"Wafer Run - Time per wafer inspection = ",
-        #"Duration = ",Replacer.ReplaceText,{"Log Message"})
+        if step2.shape[0] > 0:
 
-        step2["Log Message"] = step2["Log Message"].str.replace(
-            "Wafer Run - Time per wafer inspection = ",
-            "Duration = "
-        )
-        
-        #step5 Table.ReplaceValue(
-        #"Lot Run - Lot Inspection Done<Lot Run -Lot name : ",
-        #"Lot End <",Replacer.ReplaceText,{"Log Message"})
-        step2["Log Message"] = step2["Log Message"].str.replace(
-            "Lot Run - Lot Inspection Done<Lot Run -Lot name : ",
-            "Lot End <"
-        )
-        
-        #step6 Table.ReplaceValue(
-        #"<Wafer Run - Lot name: ",
-        #"<Wafer Run - Lot name: ",Replacer.ReplaceText,{"Log Message"})
+            #step3 Table.ReplaceValue("Lot Run - Dumping special environment variables:<Lot Run -Lot name : ",
+            #"Lot Start <",
+            #Replacer.ReplaceText,{"Log Message"})
+            step2["Log Message"] = step2["Log Message"].str.replace(
+                "Lot Run - Dumping special environment variables:<Lot Run -Lot name : ",
+                "Lot Start <"
+            )
+            
+            #step4 Table.ReplaceValue(
+            #"Wafer Run - Time per wafer inspection = ",
+            #"Duration = ",Replacer.ReplaceText,{"Log Message"})
 
-        step2["Log Message"] = step2["Log Message"].str.replace(
-            "<Wafer Run - Lot name: ",
-            "<Wafer Run - Lot name: "
-        )
-        
-        logs = step2[["Timestamp","Log Message"]]
-        
-        #step7 Table.SplitColumn("Log Message", Splitter.SplitTextByEachDelimiter({"<"}, QuoteStyle.Csv, false), {"Log Message.1", "Log Message.2"})
-        logs[["Log Message.1", "Log Message.2"]] = logs['Log Message'].str.split('<', expand=True)
-        
-        #step8 Table.ReplaceValue("Wafer Run - Lot name: ","<",Replacer.ReplaceText,{"Log Message.2"})
-        logs["Log Message.2"] = logs["Log Message.2"].str.replace(
-            "Wafer Run - Lot name: ",
-            "<"
-        )
-        
-        #step9 Table.ReplaceValue("<","",Replacer.ReplaceText,{"Log Message.2"})
-        logs["Log Message.2"] = logs["Log Message.2"].str.replace(
-            "<",
-            ""
-        )
-        
-        #step10 Table.ReplaceValue("Duration = ","",Replacer.ReplaceText,{"Log Message.1"})
-        logs["Log Message.1"] = logs["Log Message.1"].str.replace(
-            "Duration = ",
-            ""
-        )
-        
-        #step11 Table.ReplaceValue(">","",Replacer.ReplaceText,{"Log Message.2"})
-        logs["Log Message.2"] = logs["Log Message.2"].str.replace(
-            ">",
-            ""
-        )
-        
-        #Step12 Table.SplitColumn("Log Message.2", Splitter.SplitTextByDelimiter("\", QuoteStyle.Csv), {"Log Message.2.2", "Log Message.2.3"})
-        logs[["Log Message.2.1","Log Message.2.2", "Log Message.2.3"]] = logs['Log Message.2'].str.split("\\", expand=True)
-        
-        #step13 Table.SplitColumn("Log Message.2.3", Splitter.SplitTextByEachDelimiter({"Wafer ID: "}, QuoteStyle.Csv, false), {"Log Message.2.3.1", "Log Message.2.3.2"})
-        logs[["Log Message.2.3.1","Log Message.2.3.2"]] = logs['Log Message.2.3'].str.split("Wafer ID: ", expand=True)
-        
-        #step14 Table.SplitColumn("Log Message.2.3.2", Splitter.SplitTextByEachDelimiter({"Port No: "}, QuoteStyle.Csv, false), {"Log Message.2.3.2.1", "Log Message.2.3.2.2"})
-        logs[["Log Message.2.3.2.1","Log Message.2.3.2.2"]] = logs['Log Message.2.3.2'].str.split("Port No: ", expand=True)
-        
-        #step 15 
-        """
-        Table.RenameColumns({
-        #{"Log Message.2.3.2.1", "Wafer ID"}, 
-        #{"Log Message.2.3.2.2", "Port ID"}, 
-        #{"Log Message.2.3.1", "Lot ID"}, 
-        #{"Log Message.2.2", "Customer Path"}, 
-        #{"Log Message.2.1", "User Path"}, 
-        #{"Log Message.1", "Message"}})
-        """
-        logs.rename(columns = {
-            "Log Message.2.3.2.1": "Wafer ID", 
-            "Log Message.2.3.2.2": "Port ID", 
-            "Log Message.2.3.1": "Lot ID", 
-            "Log Message.2.2": "Customer Path", 
-            "Log Message.2.1": "User Path", 
-            "Log Message.1": "Message"
-        }, inplace = True)
-        
-        columns = [col for col in logs.columns if col not in ["Log Message.2","Log Message.2.3","Log Message.2.3.2"]]
-        step16 = logs[columns]
-        
-        step16["Timestamp"] = pd.to_datetime(step16.Timestamp)
-        step16["Date"] = step16['Timestamp'].dt.date
-        step16["Date"] = pd.to_datetime(step16.Date)
-        step16["Time"] = step16['Timestamp'].dt.time
-        
-        self.parsedLog = pd.concat([self.parsedLog,step16],ignore_index=True)
-        self.parsedLog.to_csv("logcompiled.csv")
+            step2["Log Message"] = step2["Log Message"].str.replace(
+                "Wafer Run - Time per wafer inspection = ",
+                "Duration = "
+            )
+            
+            #step5 Table.ReplaceValue(
+            #"Lot Run - Lot Inspection Done<Lot Run -Lot name : ",
+            #"Lot End <",Replacer.ReplaceText,{"Log Message"})
+            step2["Log Message"] = step2["Log Message"].str.replace(
+                "Lot Run - Lot Inspection Done<Lot Run -Lot name : ",
+                "Lot End <"
+            )
+            
+            #step6 Table.ReplaceValue(
+            #"<Wafer Run - Lot name: ",
+            #"<Wafer Run - Lot name: ",Replacer.ReplaceText,{"Log Message"})
+
+            step2["Log Message"] = step2["Log Message"].str.replace(
+                "<Wafer Run - Lot name: ",
+                "<Wafer Run - Lot name: "
+            )
+            
+            logs = step2[["Timestamp","Log Message"]]
+            
+            #step7 Table.SplitColumn("Log Message", Splitter.SplitTextByEachDelimiter({"<"}, QuoteStyle.Csv, false), {"Log Message.1", "Log Message.2"})
+            logs[["Log Message.1", "Log Message.2"]] = logs['Log Message'].str.split('<', expand=True)
+            
+            #step8 Table.ReplaceValue("Wafer Run - Lot name: ","<",Replacer.ReplaceText,{"Log Message.2"})
+            logs["Log Message.2"] = logs["Log Message.2"].str.replace(
+                "Wafer Run - Lot name: ",
+                "<"
+            )
+            
+            #step9 Table.ReplaceValue("<","",Replacer.ReplaceText,{"Log Message.2"})
+            logs["Log Message.2"] = logs["Log Message.2"].str.replace(
+                "<",
+                ""
+            )
+            
+            #step10 Table.ReplaceValue("Duration = ","",Replacer.ReplaceText,{"Log Message.1"})
+            logs["Log Message.1"] = logs["Log Message.1"].str.replace(
+                "Duration = ",
+                ""
+            )
+            
+            #step11 Table.ReplaceValue(">","",Replacer.ReplaceText,{"Log Message.2"})
+            logs["Log Message.2"] = logs["Log Message.2"].str.replace(
+                ">",
+                ""
+            )
+            
+            #Step12 Table.SplitColumn("Log Message.2", Splitter.SplitTextByDelimiter("\", QuoteStyle.Csv), {"Log Message.2.2", "Log Message.2.3"})
+            logs[["Log Message.2.1","Log Message.2.2", "Log Message.2.3"]] = logs['Log Message.2'].str.split("\\", expand=True)
+            
+            #step13 Table.SplitColumn("Log Message.2.3", Splitter.SplitTextByEachDelimiter({"Wafer ID: "}, QuoteStyle.Csv, false), {"Log Message.2.3.1", "Log Message.2.3.2"})
+            logs[["Log Message.2.3.1","Log Message.2.3.2"]] = logs['Log Message.2.3'].str.split("Wafer ID: ", expand=True)
+            
+            #step14 Table.SplitColumn("Log Message.2.3.2", Splitter.SplitTextByEachDelimiter({"Port No: "}, QuoteStyle.Csv, false), {"Log Message.2.3.2.1", "Log Message.2.3.2.2"})
+            logs[["Log Message.2.3.2.1","Log Message.2.3.2.2"]] = logs['Log Message.2.3.2'].str.split("Port No: ", expand=True)
+            
+            #step 15 
+            """
+            Table.RenameColumns({
+            #{"Log Message.2.3.2.1", "Wafer ID"}, 
+            #{"Log Message.2.3.2.2", "Port ID"}, 
+            #{"Log Message.2.3.1", "Lot ID"}, 
+            #{"Log Message.2.2", "Customer Path"}, 
+            #{"Log Message.2.1", "User Path"}, 
+            #{"Log Message.1", "Message"}})
+            """
+            logs.rename(columns = {
+                "Log Message.2.3.2.1": "Wafer ID", 
+                "Log Message.2.3.2.2": "Port ID", 
+                "Log Message.2.3.1": "Lot ID", 
+                "Log Message.2.2": "Customer Path", 
+                "Log Message.2.1": "User Path", 
+                "Log Message.1": "Message"
+            }, inplace = True)
+            
+            columns = [col for col in logs.columns if col not in ["Log Message.2","Log Message.2.3","Log Message.2.3.2"]]
+            step16 = logs[columns]
+            
+            step16["Timestamp"] = pd.to_datetime(step16.Timestamp)
+            step16["Date"] = step16['Timestamp'].dt.date
+            step16["Date"] = pd.to_datetime(step16.Date)
+            step16["Time"] = step16['Timestamp'].dt.time
+            
+            self.parsedLog = pd.concat([self.parsedLog,step16],ignore_index=True)
 
 
 if __name__ == "__main__":
-    path = sys.argv[1]
-    isFolder = sys.argv[2]
+    path = cfg.logfolderlocation
+    LocalCSVLocation = cfg.LocalCSVLocation
+    SharepointLocation = cfg.isFolder
+    UploadToSharePoint = cfg.UploadToSharePoint
+
+    isFolder = cfg.isFolder
     print(path)
-    parser = LogParser(path=path,isFolder = isFolder)
+    parser = LogParser(path=path,isFolder = isFolder,LocalCSVLocation = LocalCSVLocation, SharepointLocation = SharepointLocation, UploadToSharePoint = UploadToSharePoint)
+    
+
+    print("reading files from", path)
     print("first 10 rows...")
     print("========================================================================")
+
+    if UploadToSharePoint == 'N':
+        csvName = str(datetime.date.today().year) +  "KLA2925generalLogs.csv"
+
+        fullPath = LocalCSVLocation + csvName
+
+        if not isfile(fullPath):
+            parser.parsedLog.to_csv(fullPath,index=False)
+        else: # else it exists so append without writing the header
+            parser.parsedLog.to_csv(fullPath, mode='a', header=False,index=False)
+    else:
+        print("SharePoint Implem not yet finished")
+        
     print(parser.parsedLog.head(10))
     print("rows and columns",parser.parsedLog.shape)
